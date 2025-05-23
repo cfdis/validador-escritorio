3576c48d9d1440bebc72e0ccbaf27a9e05f3fd3f
EduIQ <eduiq.dev@gmail.com>
2025-05-23 00:16:03 -0600

    update(package): update version to 1.0.2 fix(db): update database path to use user data directory

--- src/main/db/client.ts
+++ src/main/db/client.ts
@@ -1,9 +1,13 @@
 import Database from 'better-sqlite3';
 import { Kysely, SqliteDialect } from 'kysely';
 import { Database as DB } from './schema';
+import { join } from 'path';
+import { app } from 'electron';
+
+const dbPath = join(app.getPath('userData'), 'validador.db');
 
 const dialect = new SqliteDialect({
-  database: new Database('validador.db'),
+  database: new Database(dbPath),
 });
 
 export const db = new Kysely<DB>({
